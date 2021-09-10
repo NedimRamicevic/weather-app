8dc58d7c0c8d3db6bdd48d0e10a467b721ad3064
Nedim <mr.ramicevic@gmail.com>
2021-09-10 08:32:17 +0300

    q

---
+++ src/components/weatherData.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {useState} from 'react'
+
+function WeatherData() {
+    
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default WeatherData
