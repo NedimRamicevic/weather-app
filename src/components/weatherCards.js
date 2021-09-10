86620b35806b39d056f0d908e7f522496262308b
Nedim <mr.ramicevic@gmail.com>
2021-09-10 08:27:11 +0300

    s

--- src/components/weatherCards.js
+++ src/components/weatherCards.js
@@ -1,16 +1,12 @@
 import React from 'react'
 
-function WeatherCards({selected}) {
+function WeatherCards({datas}) {
+    console.log(datas)
     return (
-    <div className="card" style={{width : "18rem"}}>
-        <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>            
-            <div className="degrees">
-                <h2 className="card-degree" >15</h2>
-                <h2 className="card-degree" >15</h2>
-            </div>
-        </div>
+    <div className="card-list">
+        {datas ? (
+            datas.map()
+        ):null}
     </div>
     )
 }
