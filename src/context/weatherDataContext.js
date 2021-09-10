4d5bc974d7386ede3b0ce41149db4ecbc47b3f33
Nedim <mr.ramicevic@gmail.com>
2021-09-10 09:37:13 +0300

    context

--- src/context/weatherDataContext.js
+++ src/context/weatherDataContext.js
@@ -1,3 +1,18 @@
-import {createContext} from 'react'
+import {createContext,useState} from 'react'
 const WeatherContext = createContext()
+
+const WeatherProvider = ({children})=>{
+    const [selected, setSelected] = useState("Istanbul")
+    const [weatherData, setWeatherData] = useState("nothing")
+
+  // useEffect(() => {
+  //   fetch(`http://api.openweathermap.org/data/2.5/weather?q=${selected}&units=metric&cnt=7&appid=3729444e5fb54634eff1e4e7722b1801`)
+  //   .then(x => x.json())
+  //   .then(data => {
+  //     setWeatherData(data)})
+  // }, [selected])
+    return <WeatherContext value={weatherData} >{children}</WeatherContext>
+}
+
+
 export default WeatherContext
