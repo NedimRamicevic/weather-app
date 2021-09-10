801426e99d79d1eec9c9041267e7bd1f78764d9b
Nedim <mr.ramicevic@gmail.com>
2021-09-10 12:45:18 +0300

    last

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import Dropdown from './components/dropdown'
 import WeatherCards from './components/weatherCards'
 import {useState,useEffect} from 'react'
-import WeatherContext from './context/weatherDataContext'
+import {WeatherProvider} from './context/weatherDataContext'
+import {DropdownProvider} from './context/DropdwnContext'
 import './App.css';
 
 function App() {
   const [selected, setSelected] = useState("Istanbul")
-  const [weatherData, setWeatherData] = useState()
+  const [weatherData, setWeatherData] = useState("nothing")
 
-  useEffect(() => {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${selected}&units=metric&cnt=7&appid=3729444e5fb54634eff1e4e7722b1801`)
-    .then(x => x.json())
-    .then(data => {
-      setWeatherData(data)})
-  }, [selected])
+  // useEffect(() => {
+  //   fetch(`http://api.openweathermap.org/data/2.5/weather?q=${selected}&units=metric&cnt=7&appid=3729444e5fb54634eff1e4e7722b1801`)
+  //   .then(x => x.json())
+  //   .then(data => {
+  //     setWeatherData(data)})
+  // }, [selected])
 
   const handleChange = (value) => {
     setSelected(value)
   }
   return (
     <div className="App">
-      <WeatherContext.Provider data={weatherData} handleChange={handleChange}>
-        <Dropdown /> 
+        <DropdownProvider>
+          <Dropdown />
+        </DropdownProvider>
+      <WeatherProvider>
         <WeatherCards />      
-      </WeatherContext.Provider>  
+      </WeatherProvider>
     </div>
   );
 }
